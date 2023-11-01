230090641e6e3349b0054bdc37344a5b6d79ecba
pasly0920 <inwoo920621@gmail.com>
2023-11-01 09:38:36 +0900

    style: format with ktlint

--- src/test/kotlin/com/cleancode/addonserver/AddOnServerApplicationTests.kt
+++ src/test/kotlin/com/cleancode/addonserver/AddOnServerApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class AddOnServerApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
